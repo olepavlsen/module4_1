849633e8dbfbd8c7a0c1ae71c70d189e6d29313d
olepavlsen <olepavlsen@mail.ru>
2024-08-01 11:34:27 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(x, y):
+    if y == 0:
+        return 'Ошибка'
+    else:
+        z = x / y
+    return z
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(x, y):
+    if y == 0:
+        return inf
+    else:
+        z = x / y
+    return z
